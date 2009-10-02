0f53f4eaaac4ec2a1e61b9f745b82446fc792260
mali <none@none>
2009-10-02 11:49:47 -0400

    correct bugid for jtreg tests

--- test/tools/javac/typeAnnotations/packageanno/PackageProcessor.java
+++ test/tools/javac/typeAnnotations/packageanno/PackageProcessor.java
@@ -29,7 +29,6 @@ import com.sun.source.util.TreePath;
 
 /*
  * @test
- * @bug 100079
  * @summary test that package annotations are available to type processors
  * @author Mahmood Ali
  * @compile -source 1.7 PackageProcessor.java
