6fcd403a3512660776988bb59392daca981035ad
notnoop <none@none>
2010-01-08 11:53:34 -0500

    handle type arguments to constructors (e.g. `new <@A Foo> MyClass()`).
    Such annotations were parsed, but weren't assigned proper location
    and get ignored

--- src/share/classes/com/sun/tools/classfile/ClassWriter.java
+++ src/share/classes/com/sun/tools/classfile/ClassWriter.java
@@ -696,8 +696,6 @@ public class ClassWriter {
             // new expression
             case NEW:
             case NEW_GENERIC_OR_ARRAY:
-            case NEW_TYPE_ARGUMENT:
-            case NEW_TYPE_ARGUMENT_GENERIC_OR_ARRAY:
                 out.writeShort(p.offset);
                 break;
              // local variable
@@ -752,6 +750,8 @@ public class ClassWriter {
                 out.writeByte(p.parameter_index);
                 break;
             // method type argument: wasn't specified
+            case NEW_TYPE_ARGUMENT:
+            case NEW_TYPE_ARGUMENT_GENERIC_OR_ARRAY:
             case METHOD_TYPE_ARGUMENT:
             case METHOD_TYPE_ARGUMENT_GENERIC_OR_ARRAY:
                 out.writeShort(p.offset);
