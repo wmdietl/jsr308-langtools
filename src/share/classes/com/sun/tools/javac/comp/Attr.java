6c832b46ee07dda310c94201acbe43a36ff69955
notnoop <none@none>
2010-02-18 01:38:41 -0500

    Minor optimizations for the compiler

--- src/share/classes/com/sun/tools/javac/comp/Attr.java
+++ src/share/classes/com/sun/tools/javac/comp/Attr.java
@@ -2722,6 +2722,7 @@ public class Attr extends JCTree.Visitor {
      *
      */
     public void annotateType(final Type type, final List<JCTypeAnnotation> annotations) {
+        if (annotations.isEmpty()) return;
         annotate.laterOnFlush(new Annotator() {
             @Override
             public void enterAnnotation() {
