07773cccf50ed701aca9787606244559839d0c26
Werner Dietl <wdietl@gmail.com>
2011-09-20 13:52:10 -0700

    Rename all occurrences of ExtendedAnnotation to TypeAnnotation.

--- test/tools/javap/typeAnnotations/ArrayClassLiterals.java
+++ test/tools/javap/typeAnnotations/ArrayClassLiterals.java
@@ -84,7 +84,7 @@ public class ArrayClassLiterals {
             else
                 invisibles += tAttr.annotations.length;
 
-            for (ExtendedAnnotation anno : tAttr.annotations)
+            for (TypeAnnotation anno : tAttr.annotations)
                 anno.position.toString();
         }
     }
@@ -103,7 +103,7 @@ public class ArrayClassLiterals {
             else
                 invisibles += tAttr.annotations.length;
 
-            for (ExtendedAnnotation anno : tAttr.annotations)
+            for (TypeAnnotation anno : tAttr.annotations)
                 anno.position.toString();
         }
     }
@@ -122,7 +122,7 @@ public class ArrayClassLiterals {
             else
                 invisibles += tAttr.annotations.length;
 
-            for (ExtendedAnnotation anno : tAttr.annotations)
+            for (TypeAnnotation anno : tAttr.annotations)
                 anno.position.toString();
         }
     }
