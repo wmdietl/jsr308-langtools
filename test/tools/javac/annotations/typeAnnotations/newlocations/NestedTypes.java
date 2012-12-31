b5a88f01dcac4f3aa6096a897e99a1af48134ad0
wmdietl <none@none>
2012-12-31 01:26:15 -0800

    Adapt test case to rules regarding annotations on nested types.

--- test/tools/javac/annotations/typeAnnotations/newlocations/NestedTypes.java
+++ test/tools/javac/annotations/typeAnnotations/newlocations/NestedTypes.java
@@ -45,19 +45,33 @@ class Outer {
             void m2a(@A Inner.Inner2 p2a) {}
             void m2b(Outer.@A Inner.Inner2 p2b) {}
 
-            // The location for @A is different in m3a-c 
-            void m3a(@A Outer p3a) {} // no location
-            void m3b(@A Outer.Inner p3b) {} // location [0]
-            void m3c(@A Outer.Inner.Inner2 p3c) {} // location [1]
+            // The location for @A is the same in m3a-c 
+            void m3a(@A Outer p3a) {}
+            void m3b(@A Outer.Inner p3b) {}
+            void m3c(@A Outer.Inner.Inner2 p3c) {}
+
+            // Test combinations
+            void m4a(@A Outer p3a) {}
+            void m4b(@A Outer. @B Inner p3b) {}
+            void m4c(@A Outer. @B Inner. @C Inner2 p3c) {}
         }
     }
 
-    void m4a(@A Map p4a) {} // no location
-    void m4b(@A Map.Entry p4b) {} // location [0]
-    void m4c(@A Map.@B Entry p4c) {} // @A location [0]
-    void m4d(@A Map<String,String>.@B Entry<String,String> p4d) {} // @A location [2]
-    void m4e(MyList<@A Map.Entry> p4e) {} // @A location [0,0]
-    void m4f(MyList<@A Map.@B Entry> p4f) {} // @A location [0,0]
+    void m4a(@A Map p4a) {}
+    void m4b(Map.@B Entry p4c) {}
+    // Illegal:
+    // void m4b(@A Map.Entry p4b) {}
+    // void m4c(@A Map.@B Entry p4c) {}
+
+    void m4c(Map<String,String>.@B Entry<String,String> p4d) {}
+    // Illegal:
+    // void m4d(@A Map<String,String>.@B Entry<String,String> p4d) {}
+
+    void m4e(MyList<Map.Entry> p4e) {}
+    void m4f(MyList<Map.@B Entry> p4f) {}
+    // Illegal:
+    // void m4g(MyList<@A Map.Entry> p4e) {}
+    // void m4h(MyList<@A Map.@B Entry> p4f) {}
 
     class GInner<X> {
         class GInner2<Y, Z> {}
@@ -76,13 +90,9 @@ class Test1 {
     // Outer.@A GStatic.GStatic2<Object> gsbad;
 
     MyList<@A Outer . @B Inner. @C Inner2> f;
-    // @A location: [4]
     @A Outer .GInner<Object>.GInner2<String, Integer> g;
 
-    // TODO: Put @A on the type, not the package, maybe.
-    //MyList<@A java.lang.Object> pkg;
-
-    // Make sure that something like this fails gracefully: 
+    // TODO: Make sure that something like this fails gracefully: 
     // MyList<java.@B lang.Object> pkg;
 
     @A Outer f1;
@@ -90,79 +100,59 @@ class Test1 {
     // TODO: ensure type annos on new are stored.
     @A Outer . @B GInner<@C Object> f3 = f1.new @B GInner<@C Object>();
 
-    // @A location [0, 3]
-    // @B location [0, 2]
-    // @C location [0, 2, 0]
-    // @D location [0, 2, 0, 0]
-    // @E location [0] 
-    // @F location [0, 0]
-    // @G location [0, 1]
     MyList<@A Outer . @B GInner<@C MyList<@D Object>>. @E GInner2<@F Integer, @G Object>> f4;
     // MyList<Outer.GInner<Object>.GInner2<Integer>> f4clean;
 
-    // @A location [3]
-    // @B location [2]
-    // @C location [2, 0]
-    // @D location [2, 0, 0]
-    // @E on field 
-    // @F location [0]
-    // @G location [1]
     @A Outer . @B GInner<@C MyList<@D Object>>. @E GInner2<@F Integer, @G Object> f4top;
 
-    // @A location [0, 1, 3]
-    // @B location [0, 1, 2]
-    // @C location [0, 1, 2, 0]
-    // @D location [0, 1, 2, 0, 0, 1]
-    // @E location [0, 1, 2, 0, 0]
-    // @F location [0, 1, 2, 0, 0, 0]
-    // @G location [0, 1]
-    // @H location [0, 1, 0]
-    // @I location [0, 1, 1]
-    // @J location [0]
-    // @K location [0, 0]
     MyList<@A Outer . @B GInner<@C MyList<@D Object @E[] @F[]>>. @G GInner2<@H Integer, @I Object> @J[] @K[]> f4arr;
 
-    // @A location [1, 3]
-    // @B location [1, 2]
-    // @C location [1, 2, 0]
-    // @D location [1, 2, 0, 0, 1]
-    // @E location [1, 2, 0, 0]
-    // @F location [1, 2, 0, 0, 0]
-    // @G location [1]
-    // @H location [1, 0]
-    // @I location [1, 1]
-    // @J on field
-    // @K location [0]
     @A Outer . @B GInner<@C MyList<@D Object @E[] @F[]>>. @G GInner2<@H Integer, @I Object> @J[] @K[] f4arrtop;
 
-    MyList<@A Outer . @B Static> f5;
-    @A Outer . @B Static f6;
-    @Av("A") Outer . @Bv("B") GStatic<@Cv("C") String, @Dv("D") Object> f7;
-    @A Outer . @Cv("Data") Static f8;
-    MyList<@A Outer . @Cv("Data") Static> f9;
+    MyList<Outer . @B Static> f5;
+    // Illegal:
+    // MyList<@A Outer . @B Static> f5;
+
+    Outer . @B Static f6;
+    // Illegal:
+    // @A Outer . @B Static f6;
+
+    Outer . @Bv("B") GStatic<@Cv("C") String, @Dv("D") Object> f7;
+    // Illegal:
+    // @Av("A") Outer . @Bv("B") GStatic<@Cv("C") String, @Dv("D") Object> f7;
+
+    Outer . @Cv("Data") Static f8;
+    // Illegal:
+    // @A Outer . @Cv("Data") Static f8;
+
+    MyList<Outer . @Cv("Data") Static> f9;
+    // Illegal:
+    // MyList<@A Outer . @Cv("Data") Static> f9;
 }
 
 class Test2 {
     void m() {
         @A Outer f1 = null;
         @A Outer.@B Inner f2 = null;
-        @A Outer.@B Static f3 = null;
+        Outer.@B Static f3 = null;
+        // Illegal:
+        // @A Outer.@B Static f3 = null;
         @A Outer.@C Inner f4 = null;
 
-        @A Outer . @B Static f5 = null;
-        @A Outer . @Cv("Data") Static f6 = null;
-        MyList<@A Outer . @Cv("Data") Static> f7 = null;
+        Outer . @B Static f5 = null;
+        Outer . @Cv("Data") Static f6 = null;
+        MyList<Outer . @Cv("Data") Static> f7 = null;
     }
 }
 
 class Test3 {
     void monster(@A Outer p1,
         @A Outer.@B Inner p2,
-        @A Outer.@B Static p3,
+        Outer.@B Static p3,
         @A Outer.@Cv("Test") Inner p4,
-        @A Outer . @B Static p5,
-        @A Outer . @Cv("Data") Static p6,
-        MyList<@A Outer . @Cv("Data") Static> p7) {
+        Outer . @B Static p5,
+        Outer . @Cv("Data") Static p6,
+        MyList<Outer . @Cv("Data") Static> p7) {
     }
 }
 
@@ -170,11 +160,14 @@ class Test4 {
     void m() {
         @A Outer p1 = new @A Outer();
         @A Outer.@B Inner p2 = p1.new @B Inner();
-        @A Outer.@B Static p3 = new @A Outer.@B Static();
+        // Illegal:
+        // @A Outer.@B Static p3 = new @A Outer.@B Static();
+        // Object o3 = new @A Outer.@B Static();
+
         @A Outer.@Cv("Test") Inner p4 = p1.new @Cv("Test") Inner();
-        @A Outer . @B Static p5 = new @A Outer . @B Static();
-        @A Outer . @Cv("Data") Static p6 = new @A Outer . @Cv("Data") Static();
-        MyList<@A Outer . @Cv("Data") Static> p7 = new MyList<@A Outer . @Cv("Data") Static>();
+        Outer . @B Static p5 = new Outer . @B Static();
+        Outer . @Cv("Data") Static p6 = new Outer . @Cv("Data") Static();
+        MyList<Outer . @Cv("Data") Static> p7 = new MyList<Outer . @Cv("Data") Static>();
     }
 }
 
