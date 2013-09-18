8c7608186521a9243eceaeb6f3583115e6529011
wmdietl <none@none>
2013-09-18 15:48:54 -0700

    JDK-8024961: don't perform admissibility check if annotation type is erroneous.

--- test/tools/javac/annotations/typeAnnotations/failures/Scopes.java
+++ test/tools/javac/annotations/typeAnnotations/failures/Scopes.java
@@ -8,10 +8,5 @@
 class Scopes<T extends @UniqueInner Object> {
   // UniqueInner is not visible in the type parameters.
   // One has to use Scopes.UniqueInner.
-  // Annotations with the default @Target are not allowed there,
-  // so we also get the second error about the invalid location.
-  // Adding the target here doesn't matter, as we don't resolve
-  // the annotation type.
-  // @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
   @interface UniqueInner { };
 }
