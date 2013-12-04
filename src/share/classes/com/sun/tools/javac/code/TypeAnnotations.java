118f2e07e74461fed65a1c9801376c7f5cefbcb7
wmdietl <none@none>
2013-12-04 18:36:53 -0500

    Do not create an unused, local Options instance.

--- src/share/classes/com/sun/tools/javac/code/TypeAnnotations.java
+++ src/share/classes/com/sun/tools/javac/code/TypeAnnotations.java
@@ -72,7 +72,6 @@ import com.sun.tools.javac.util.List;
 import com.sun.tools.javac.util.ListBuffer;
 import com.sun.tools.javac.util.Log;
 import com.sun.tools.javac.util.Names;
-import com.sun.tools.javac.util.Options;
 
 /**
  * Contains operations specific to processing type annotations.
@@ -105,7 +104,6 @@ public class TypeAnnotations {
         syms = Symtab.instance(context);
         annotate = Annotate.instance(context);
         attr = Attr.instance(context);
-        Options options = Options.instance(context);
     }
 
     /**
