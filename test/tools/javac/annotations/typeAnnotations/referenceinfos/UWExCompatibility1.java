ccd762c94a85344fb9f09b478838af4f53983c79
Werner Dietl <wdietl@gmail.com>
2014-07-28 16:56:35 -0400

    Test case for type annotations in comments with Java 6.

---
+++ test/tools/javac/annotations/typeAnnotations/referenceinfos/UWExCompatibility1.java
@@ -0,0 +1,35 @@
+/*
+ * Test of UW local extension: type annotations in comments with Java 6.
+ */
+
+/*
+ * @test
+ * @bug 1111111
+ * @summary Java 6: type annotations allowed with annotations in comments
+ * @compile -g Driver.java ReferenceInfoUtil.java
+ * @compile -source 6 -target 6 UWExCompatibility1.java
+ * @run main Driver UWExCompatibility1
+ * @author Werner Dietl
+ */
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+import static com.sun.tools.classfile.TypeAnnotation.TargetType.*;
+
+import java.util.List;
+
+@Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER })
+@interface TA {}
+
+public class UWExCompatibility1 {
+    @TADescription(annotation = "TA", type = FIELD, genericLocation = { 3, 0 })
+    public String typeAnnoInComment() {
+        return "List</*@TA*/ Object> f;";
+    }
+
+    @TADescription(annotation = "TA", type = FIELD, genericLocation = { 3, 0 })
+    public String typeAnnoInVoodooComment() {
+        return "List</*>>> @TA */ Object> g;";
+    }
+}
