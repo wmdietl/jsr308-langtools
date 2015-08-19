54a8384a3ed24cbee2e7527d72afbc2a68e6bfb1
Werner Dietl <wdietl@gmail.com>
2015-08-19 01:17:48 -0400

    Move triggering down-stream projects to .travis.yml.

--- .travis-build.sh
+++ .travis-build.sh
@@ -2,8 +2,3 @@
 cd make
 ant
 
-echo Triggering build of typetools/annotation-tools
-curl -s https://raw.githubusercontent.com/mernst/plume-lib/master/bin/trigger-travis.sh > trigger-travis.sh
-bash trigger-travis.sh typetools annotation-tools $TRAVISTOKEN
-rm trigger-travis.sh
-
